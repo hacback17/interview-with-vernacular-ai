0fce4d06bdb957efcff8264343eb8167499d3158
Bablu Kumar <hacback17@gmail.com>
2020-12-10 19:43:11 +0530

    All code done

---
+++ managing-disk.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/bash
+
+: '
+We provide call center automation as one of our products. Essentially it processes user"s audio and return a correct audio response. For managing all these audios that are generated in the due process we use NFS. With the call volumes we have, it essential that disk space of NFS should be managed properly.
+Write a bash script to clean out all the audios in /home/ubuntu/audios/ folder that are older than 48hrs. Also generate a log file with the name deleted-files-<date>-<month>-<year>.log which should contain:
+    name of audio file
+    time of creation of audio file (ISO Format)
+    time of deletion of audio file (ISO Format)
+For example: A sample log file deleted-files-12-02-2020.log will contain:
+audio_0010023.wav 10-02-2020T08:37:16+05:30 12-02-2020T10:18:40+05:30
+audio_0010024.wav 10-02-2020T08:38:16+05:30 12-02-2020T10:18:42+05:30
+audio_0010025.wav 10-02-2020T08:38:50+05:30 12-02-2020T10:18:45+05:30
+'
+
+if [ "$EUID" -ne 0 ]
+	then echo "Please run as root"
+	exit
+	else
+
+	clear
+	echo "Loading info."
+	sleep 1
+	clear
+	echo "Loading info.."
+	sleep 1
+	clear
+	echo "Loading info..."
+	clear
+
+	echo -e "\e[1;42m Managing Disk Space... \e[0m"
+
+	output_path=`pwd` # current working directory
+	fpath='/home/osboxes/audios/' # the directory where audios are stored
+	cd $fpath
+	echo `pwd`
+	find $fpath -type f -mtime +2 -exec ls -l {} \; > output          					# output the file older than 48hrs
+	cat output | awk -F " " '{print $9}' | awk -F "/" '{ print $NF }' > output_name 	#Print the file name
+	find $fpath -type f -mtime +2 -exec date -r {} --iso-8601=seconds \; > output_file_creation_iso_time
+
+	cat output_name
+	cat output_file_creation_iso_time
+
+	paste output_name output_file_creation_iso_time | while read output_name output_file_creation_iso_time; do
+		echo "$output_name $output_file_creation_iso_time $(date --iso-8601=seconds)" >> $output_path/deleted-files-$(date +%m-%d-%Y).log
+	done
+
+	find $fpath -type f -mtime +2 -delete # delete the files
+fi
+
+
+# find / -type f -mtime +2 -print0 2>/dev/null | tail --zero-terminated -n 15 | xargs --null cp -p -t $fpath               # generate some file
---
+++ sysaudit.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/bash
+
+: '
+Auditing Hardware
+We work with companies that require our solution to be deployed on premise. For that we receive hardware, and auditing that hardware becomes important part of our deployment process.
+Write a bash script to audit the following hardware spec in RHEL. The script should print out following specifications:
+    OS version
+    Kernel version
+    CPU - Virtual cores (ratio to physical cores if possible)
+    CPU - Clock speed
+    CPU - Architecture
+    RAM
+    Networking - Bandwidth
+    Networking - OS Firewall (Allowed Ports & Protocols)
+    Networking - Network Firewall (Allowed Ports & Protocols)
+    Disk - Mounted/Unmounted volumes, type, storage
+'
+
+if [ "$EUID" -ne 0 ]
+	then echo "Please run as root"
+	exit
+	else
+
+	clear
+	echo "Loading info."
+	sleep 1
+	clear
+	echo "Loading info.."
+	sleep 1
+	clear
+	echo "Loading info..."
+	clear
+
+	echo -e "\e[1;42m Downloading Required Packages. Please wait... \e[0m"
+	sudo yum install --enablerepo="epel" ufw -y  # install ufw
+	echo "y" | sudo ufw enable 		     # enable ufw
+	sudo yum -y install iftop  		     # iftop
+	clear
+
+	#echo "###Auditing Hardware###"
+	echo -e "\e[1;42m Auditing Hardware \e[0m"
+
+	# System Info
+	echo -e "\e[1;31m###System Info### \e[0m"
+	echo "OS: "$(cat /etc/os-release | egrep '^NAME=' | cut -d '"' -f 2) $(cat /etc/os-release | egrep '^VERSION=' | cut -d '"' -f 2)
+	echo "Kernel Version: "$(uname -a | cut -d " " -f 3)
+	echo
+
+	# CPU Info
+	echo -e "\e[1;31m###CPU Info### \e[0m"
+	echo "CPU Cores:" $(grep -c ^processor /proc/cpuinfo)
+	echo "CPU Clock Speed:" $(lscpu | grep MHz | cut -d ":" -f 2 | xargs)" MHz"
+	echo "CPU Architecture:" $(lscpu | grep Architecture | cut -d ":" -f 2 | xargs)
+	echo
+
+	# RAM Info
+	echo -e "\e[1;31m###RAM Info### \e[0m" 
+	echo "Total RAM:" $(free -mh | grep Mem | awk -F " " '{ print $2 }'), "Used RAM:" $(free -mh | grep Mem | awk -F " " '{ print $3 }'), "Free RAM:" $(free -mh | grep Mem | awk -F " " '{ print $4 }')
+	echo
+
+	# Network Bandwidth Info
+	echo -e "\e[1;31m###Network Bandwidth Info### \e[0m" 
+	nohup ping google.com -c 4 2>1& 
+
+	total_network_bandwidth=$(sudo iftop -t -s 1 -n -N 2>/dev/null | awk '/send and receive/ {print $8}')
+
+	echo "Network bandwidth": $total_network_bandwidth
+
+	# Ports info using IFTOP
+	#echo $(sudo iftop -t -s 1 -n -N 2>/dev/null | awk '/send and receive/ {gsub(/[^0-9]?b$/,"",$8) ; print $8}')
+
+	# Ports using UFW
+	printf "\nAllowed Ports:\n"
+
+	sudo ufw status verbose | sed '1,7d'
+
+	#echo $(sudo ufw status verbose | sed '1,7d' | awk -F / '{ print $1 }' | awk -F " " '{ print $NF }')
+
+	echo "Allowed Protocols: " $(firewall-cmd --list-services)
+	echo
+
+	echo -e "\e[1;31m###Disk Info### \e[0m" 
+
+	lsblk -o NAME,FSTYPE,SIZE,MOUNTPOINT
+	echo -e "\e[1;42m System audit successfully completed. \e[0m"
+fi
